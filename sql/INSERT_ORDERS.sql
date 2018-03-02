c6703d00c1a12eb783dba65e29e59e1498b375fd
Сергей <bovasergey48@gmail.com>
2018-03-02 02:03:58 +0200

    ADD LESSON 3

---
+++ sql/INSERT_ORDERS.sql
@@ -0,0 +1,29 @@
+INSERT INTO ORDERS
+VALUES(1005, 'test1', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1006, 'test2', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1007, 'test3', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1008, 'test4', 110, TO_DATE('2017/06/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/07/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1009, 'test5', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1010, 'test6', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1011, 'test7', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1012, 'test8', 110, TO_DATE('2017/06/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/07/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1013, 'test9', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(1014, 'test10', 140, TO_DATE('2017/05/03 21:00:00','yyyy/mm/dd hh24:mi:ss'), TO_DATE('2017/05/03 23:00:00','yyyy/mm/dd hh24:mi:ss'));
